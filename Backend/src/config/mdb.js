a3938e2ff0ae5e780a5a98a18b8963d368a77ad0
Aravind <aravinths921@gmail.com>
2026-03-21 00:14:00 +0530

    server update

--- Backend/src/config/mdb.js
+++ Backend/src/config/mdb.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectdb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Database connected");
-  } catch (err) {
-    console.error("Database not connected");
-    console.error(err);  // <-- log the real error
-  }
-};
-
+const connectdb = async()=>{
+    try{
+       await mongoose.connect(process.env.MONGO_URI);
+       console.log("Database connected")}
+       catch(err){(console.log("Database not connected"))
+    } 
+}
+                                             
 module.exports = connectdb;
+    
