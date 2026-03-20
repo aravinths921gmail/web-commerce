076543253c8076ba4899033fb7958f5faefcb298
Aravind <aravinths921@gmail.com>
2026-03-20 12:34:07 +0530

    server update

--- Backend/Server.js
+++ Backend/Server.js
@@ -8,7 +8,7 @@ const redis = require("./src/config/redis");
 (async () => {
   try {
     await connectdb(); //Only after db connect, server starts listen. If server runs when db fails, hacker send ddos login data to crash server
-    App.listen(process.env.PORT, () => console.log("Server running")); //6th
+    App.listen(process.env.PORT, "0.0.0.0", () => console.log("Server running")); //6th
   } catch (err) {
     console.error(err.message);
   }
